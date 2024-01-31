1627e452517908c44c9a17eaf40a49f3cc72924a
MikeRotchip <p3tr.kada@gmail.com>
2024-01-31 23:03:40 +0100

    nextjs-example

---
+++ nextjs-example/src/app/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { MAP } from "./public-map";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <span>
+        Corporate wants you to find the difference between these 2 pictures:
+      </span>
+      <div className={styles.container}>
+        <Image
+          // NextJS requires a leading slash on relative paths
+          src={`/${MAP.images["bus_1.gif"]}`}
+          alt={"bus"}
+          width={300}
+          height={300}
+        />
+        <Image
+          // NextJS requires a leading slash on relative paths
+          src={`/${MAP.images["bus_2.gif"]}`}
+          alt={"bus"}
+          width={300}
+          height={300}
+        />
+      </div>
+
+      <details>
+        <summary></summary> They are the same picture
+      </details>
+    </main>
+  );
+}
---
+++ nextjs-example/src/map-public-folder.ts
@@ -0,0 +1,15 @@
+import path from "node:path";
+import { exportFolderMap } from "folder-mapper-ts";
+
+const main = async () => {
+  await exportFolderMap({
+    // Absolute path to public folder
+    path: path.join(__dirname, "..", "public"),
+    // Absolute path to output file in /src/app/public-map.ts
+    outputPath: path.join(__dirname, "app", "public-map.ts"),
+    // Absolute path to public folder
+    filePathsRelativeTo: path.join(__dirname, "..", "public"),
+  });
+};
+
+main();
